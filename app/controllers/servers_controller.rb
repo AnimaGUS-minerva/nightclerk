fdde9517d104a4ca5746278abf30c42e296a3c29
Michael Richardson <mcr@sandelman.ca>
2023-05-03 14:57:20 -0400

    fixup! 576c586d8c26b35e84f488b8a1a04e7d48471d25

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,2 +1,20 @@
+require 'byebug'
+
 class ServersController < ApplicationController
+
+  def create
+    appliance = Server.new
+    appliance.requesting_address = request.env["REMOTE_ADDR"]
+    unless params[:email]
+      head 404, :text => "Missing email"
+      return
+    end
+
+    owner = Owner.find_or_create_by(:email => params[:email])
+    appliance.owner = owner
+    appliance.save!
+
+    head 201, :location => url_for(appliance)
+  end
+
 end
